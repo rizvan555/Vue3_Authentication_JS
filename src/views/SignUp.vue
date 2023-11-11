c3d05cae897aef368929a9e681abe103dc7e82dc
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-11-11 12:12:13 +0100

    added function for errors and used Yup for forms criters for emails too

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -92,7 +92,9 @@ export default {
     });
 
     const schema = Yup.object().shape({
-      username: Yup.string().required('Username is required'),
+      username: Yup.string()
+        .min(3, 'Username must be at least 3 characters')
+        .required('Username is required'),
       email: Yup.string().email('Invalid email').required('Email is required'),
       password: Yup.string()
         .min(6, 'Password must be at least 6 characters')
